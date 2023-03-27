605d7070bbf48f8d54a76e53ef21da861008dfe4
Fidelin Hugo <Vous@exemple.com>
2023-03-27 10:48:10 +0200

    Update error & readme

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Codes Python réalisés lors de mon stage de master sur la mise enplace d'un solveur pour matrice H2.
 
+## Installation de $h2tools$
+
 Pour ces codes, il est nécessaire d'installer différents package Python :
 
 * pip 
@@ -15,13 +17,37 @@ Pour ces codes, il est nécessaire d'installer différents package Python :
 
 Un tutorial est (en cours) disponible dans le rapport de ce stage.
 
-[A COMPLETER]
 
-Il est à noter que pour utiliser *scipy.sparse.linalg.gmres* il faut ajouter un un attribut dans le fichier $h2matrix.py$. Il faudra ajouter :
+
+## Analyse du produit matrice-vecteur pour une matrice $\mathcal{H} ^ 2$
+
+La classe 'H2Matrix' contient déjà un produit scalaire matrice-vecteur nommé $.dot()$. Il est alors intéressant d'étudier l'erreur commis par cette opération pour une matrice aléatoire de taille $N \times N$, et en fonction de $\tau > 0$
+
+
+![erreur_mat_vec](./Images/Err_Prod_Mat_Vec_log_N.png)
+
+On lit alors qu'en choisissant $\tau < 10{-5}$ on obtient l'erreur la plus faible possible. Dans le reste des calculs, on prendra alors $\mathbf{\tau = 10^{-5}}$ 
+
+## Solveur itératif de Krylov
+
+La classe 'H2Matrix' étant un format data-sparse, elle ne donne accès qu'à très peu d'information quant à la matrice. Comme on l'a vu plus haut, cette classe embarque un produit matrice-vecteur, néanmoins aucune autre opérations n'est disponible. Il est donc sensé d'utiliser des algorithmes itératifs dans l'objectif de résoudre :
+
+$$ Ax = b $$
+
+Avec :
+* $A$ matrice $\mathcal{H} ^ 2 (\mathbb{R} ^ {N \times N})$
+* $b\in\mathbb{R} ^ {N }$ vecteur source 
+* $x\in\mathbb{R} ^ {N }$ vecteur solution
+
+Nous allons nous intéresser aux solveurs itératifs de Krylov, en particulier les algorithmes GMRES car ils ne nécessitent que d'un produit matrice-vecteur pour fonctionner.
+
+Dans un premier temps, utilisons la fonction *scipy.sparse.linalg.gmres* ( ou une de ses variantes *scipy.sparse.linalg.lgmres*) afin de réaliser un tel algorithme.
+
+Il est à noter que pour utiliser ces fonctions il faut rajouter un un attribut dans le fichier $h2matrix.py$. Il faudra ajouter :
 
 ```
 def mat_vec(self,x):
 	return self.dot(x)
 ```
 
-Afin que * scipy * puisse utiliser un produit matrice-vecteur.
+Afin que *scipy* puisse utiliser un produit matrice-vecteur.
--- Test/Err_ Mat_Vec.py
+++ Test/Err_ Mat_Vec.py
@@ -5,71 +5,49 @@ from h2tools.collections import particles
 from h2tools import ClusterTree
 from h2tools import Problem
 from h2tools.mcbh_2 import mcbh
+from functions import init_particules_problem
 
-
-def init_A(position):
-    ndim, N = position.shape
-    A = np.zeros((N,N))
-    for i in range(N):
-        for j in range(N):
-            if i != j :
-                Xi = np.array([position[:, i]])
-                Xj = np.array([position[:, j]])
-                r = np.linalg.norm(Xi - Xj)
-                A[i, j] = 1 / r
-    return A
 
 if __name__ == "__main__":
 
     np.random.seed(0)
     
     start = time.time()
-    N = 500
-    ndim = 3
-    count = N
-    position = np.random.randn(ndim, count)
+    N_vec = [2000, 3000, 4000, 5000]
+    for N in N_vec:
+        ndim = 3
+        count = N
+        position = np.random.randn(ndim, count)
 
-    
-    data = particles.Particles(ndim, count, vertex=position)
-    tree = ClusterTree(data, block_size=25)
-    func = particles.inv_distance
-    problem = Problem(func, tree, tree, symmetric=1, verbose=0)
-    
-    A = init_A(position)
-    A_h2 = mcbh(problem, tau=1e-4, iters=0, verbose=0)
+        
+        func = particles.inv_distance
+        problem, A = init_particules_problem(position, func, full_matrix=True)
+        
+        A_h2 = mcbh(problem, tau=1e-4, iters=0, verbose=0)
 
-    X = np.zeros(N)
-    X[0] = 1
-    X_err = [(1e-1 ** i) for i in range(1,15)]
-    Y_err = []
-    res = A.dot(X)
-    for t in X_err :
-        print(chr(964), f"= {t}")
-        A_h2 = mcbh(problem, tau=t, iters=1, verbose=0)
-        A_h2.svdcompress(1e-4)
-        res_h2 = A_h2.dot(X)
-        print(np.linalg.norm(res ), np.linalg.norm(res_h2), "\n")
-        err = np.linalg.norm(res - res_h2)
-        Y_err.append(err)
-    
-    print(f"Temps d'exécution : {time.time() - start}")
-    
-   
-    
-    plt.plot(X_err, Y_err, c='r')
-    plt.title(r"Erreur commise pour $\tilde{y} = \tilde{A} x$ avec $N=5000$")
-    plt.xlabel(r"Valeurs de $\tau$")
-    plt.ylabel(r"Valeurs de $|| y - \tilde{y}||_2$")
+        X = np.zeros(N)
+        X[0] = 1
+        X_err = [(1e-1 ** i) for i in range(1,15)]
+        Y_err = []
+        res = A.dot(X)
+        for t in X_err :
+            print(chr(964), f"= {t}")
+            A_h2 = mcbh(problem, tau=t, iters=1, verbose=0)
+            A_h2.svdcompress(1e-4)
+            res_h2 = A_h2.dot(X)
+            print(np.linalg.norm(res ), np.linalg.norm(res_h2), "\n")
+            err = np.linalg.norm(res - res_h2)
+            Y_err.append(err)
+        
+        #print(f"Temps d'exécution : {time.time() - start}")
+        
+        
+        plt.loglog(X_err, Y_err, label=f"N={N}", linewidth=2)
+        plt.title(r"Erreur commise pour $\tilde{y} = \tilde{A} x$")
+        plt.xlabel(r"Valeurs de $\tau$")
+        plt.ylabel(r"Valeurs de $|| y - \tilde{y}||_2$")
+        plt.legend()
     plt.grid()
     plt.show()
-
-    plt.clf()
-
-    plt.loglog(X_err, Y_err, c ='r')
-    plt.title(r"Erreur commise pour $\tilde{y} = \tilde{A} x$ avec $N=5000$")
-    plt.xlabel(r"Valeurs de $\tau$")
-    plt.ylabel(r"Valeurs de $|| y - \tilde{y}||_2$")
-    plt.grid()
-    plt.show()
-     
-    
+        
+        

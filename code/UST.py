914721fbb8dbde1b09f34b8de182b5b4e3b4855b
Fidelin Hugo <hfidelin@condat.laplace.enseeiht.fr>
2023-07-13 17:12:31 +0200

    implement A1

--- code/UST.py
+++ code/UST.py
@@ -18,16 +18,18 @@ from functions import *
 if __name__ == '__main__':
     
     start = time.time()
-
-    N = 2 ** 3
+    N = 2 ** 5
     ndim = 1
     position = np.linspace(0, 1, N).reshape(ndim, N)
     #position = np.random.randn(ndim, N)
+    L = 4
+    
+    tau = 1e-3
+    block_size = N // (2 ** L)
 
-    tau = 1e-2
-    block_size = 2
     func = particles.inv_distance
-    problem, L, A = init_particules_problem(position, func, block_size=block_size, full_matrix=True)
+    problem, L, A = init_particules_problem(position, func, block_size=block_size, 
+                                               full_matrix=True)
     
     print(70 * '-')
     print(f"DONNÉES DU PROBLÈME :")
@@ -39,27 +41,42 @@ if __name__ == '__main__':
     print(70 * '-', '\n')
 
     A_h2 = mcbh(problem, tau, iters=1, verbose=0 )
-    A_h2.svdcompress(1e-9)
+    A_h2.svdcompress(tau)
     row_interact = A_h2.row_interaction
     col_interact = A_h2.col_interaction
 
     row_transfer = A_h2.row_transfer
     col_transfer = A_h2.col_transfer
 
-    row_leaf, col_leaf = init_list_leaf(row_transfer, col_transfer, Block_size=block_size)
-    print(row_leaf)
-    C0 = init_C0(N, problem)
-    U0 = init_U0(N, row_leaf, Block_size=block_size)
+    row_basis = init_vect_base(problem, row_transfer)
+    col_basis = init_vect_base(problem, col_transfer)
     
-    print(f"\nTemps d'exécution : {time.time() - start}")
+    
+    print(70 * '-', '\n')
+    vect_U = []
+    vect_V = []
+    for l in reversed(range (1, L + 1)):
+        print(f"Level : {L + 1 - l}")
+        shape = (2 ** l, 2 ** l)
+        print(f"Block shape : {shape}")
+        vect_l = []
+        for j in range(1, len(row_basis)):
+            base = row_basis[j]
+            if base.shape[0] == shape[0]:
+                vect_l.append(base) 
+        U = init_U0(N, vect_l, 2 ** l)
+        V = init_V0(N, vect_l, 2 ** l)
+        
+        vect_U.append(U)
+        vect_V.append(V)
+    
 
-    plt.imshow(U0.todense())
+    A_h2 = la.LinearOperator((N, N), matvec=A_h2.dot, rmatvec=A_h2.rdot)
+    
+    A1 = vect_U[-1].T @ A_h2.matmat(vect_V[-1].todense())
+    plt.imshow(A1)
     plt.colorbar()
     plt.show()
-    
-    #plt.matshow(A_plot)
-    #plt.colorbar()
-    #plt.show()
     
     
         

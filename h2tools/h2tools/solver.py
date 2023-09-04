470e2323c6efa7efbe6b05c7fea7fa8a7a43be31
hfidelin <100036431+hfidelin@users.noreply.github.com>
2023-09-04 11:55:28 +0200

    Update solver.py

--- h2tools/h2tools/solver.py
+++ h2tools/h2tools/solver.py
@@ -6,6 +6,8 @@ from .h2_to_sparse import convert_h2_to_sparse
 
 
 
+#quick class to print gmres evolution during computing
+
 class gmres_counter(object):
     def __init__(self, disp=True):
         self._disp = disp
@@ -18,7 +20,18 @@ class gmres_counter(object):
 
 
 def sparse_matrix(h2_matrix):
+    """
+    Compute matrices from the sparse factorisation for direct solver
 
+    Parameter:
+    ----------
+
+    h2_matrix : python object 'H2matrix' class
+
+    Return:
+    S, U, V :   _ S csc sparse matrix 
+                _ U, V csc sparse orthogonal matrices
+    """
 
     row_tree = h2_matrix.problem.row_tree
     level_count = len(row_tree.level)-1
@@ -36,9 +49,27 @@ def sparse_matrix(h2_matrix):
     
     
 
-    return S, U_mat, V_mat
+    return S.tocsc(), U_mat.tocsc(), V_mat.tocsc()
 
 def direct_solver(h2_matrix, b):
+    """
+    Compute the solution x of the linear system from h2_matrix and the vector b
+    using direct method from sparse factorisation
+
+    Parameters:
+    ----------
+
+    h2_matrix: python object 'H2matrix' class
+    
+    b : ndarray
+        column vector used as right hand side of the linear system
+
+    Returns:
+    ----------
+
+    x : ndarray
+        solution of the linear system
+    """
 
     S, U, V = sparse_matrix(h2_matrix)
 
@@ -51,13 +82,39 @@ def direct_solver(h2_matrix, b):
     return x
 
 
-def gmres_solver(h2_matrix, b, eps, M=None):
+def gmres_solver(h2_matrix, b, eps=1e-5, M=None):
+    """
+    Compute the solution x of the linear system from h2_matrix and the vector b
+    using GMRES algorithm from Scipy
+
+    Parameters:
+    ----------
+
+    h2_matrix: python object 'H2matrix' class
     
+    b : ndarray
+        column vector used as right hand side of the linear system
+
+    eps : float
+        tolerance for convergence
+
+    M : sparse matrix, ndarray, LinearOperator
+        inverse of the preconditionneur of h2_matrix (see notes from Scipy GMRES)
+
+    Returns:
+    ----------
+
+    x : ndarray
+        approximation of the solution of the linear system
+    """
+
+    count = gmres_counter()
     N = h2_matrix.shape[0]
     restart = int(N / 2)
-    x, info = sp.linalg.gmres(h2_matrix, b, tol=eps,restart=restart, maxiter=300, M=M )
+    x, info = sp.linalg.gmres(h2_matrix, b, tol=eps,restart=restart, maxiter=300, M=M, callback=count )
     return x
 
+"""
 def inv_precond(h2_matrix):
     print("Solver appelé")
     S, U, V = sparse_matrix(h2_matrix)
@@ -68,4 +125,4 @@ def inv_precond(h2_matrix):
     P_linop = sp.linalg.LinearOperator(P.shape, Px)
     print("Solveur renvoi effectué")
     return P_linop
-
+"""
